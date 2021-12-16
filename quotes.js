128d171ffa8a6a93c7775b3f4dbc9b64fd440550
lespuch-v <36127590+lespuch-v@users.noreply.github.com>
2021-12-16 10:47:55 +0100

    loading additional quotes

--- quotes.js
+++ quotes.js
@@ -1,14 +1,22 @@
-const qArray = ["There is no fixed truth about how you are as person.", 
-"Your thought are structuring the experience of the word.",
-"You dont have the problem that you think you have. ",
-"Have you noticed this... tha you never quite arrive and every goal / desire you achieve is like a mirrage. ",
-"Remember! You will DIE!",
-"Rely on yourself!",
-"NO ZERO DAYS",
-"Do things over and over and over again. REPETITION is the KEY!",
-"Sacrifice NOW to gain later.",
-"Its all in your head.",
-"Celebrate WINS but not ALL of them."
-]
+const qArray = [
+  "Your thought are structuring the experience of the word.",
+  "You dont have the problem that you think you have. ",
+  "Have you noticed this... tha you never quite arrive and every goal / desire you achieve is like a mirrage. ",
+  "Remember! You will DIE!",
+  "Rely on yourself!",
+  "NO ZERO DAYS",
+  "Do things over and over and over again. REPETITION is the KEY!",
+  "Sacrifice NOW to gain later.",
+  "Its all in your head.",
+  "Celebrate WINS but not ALL of them.",
+  "Be yourself; everyone else is already taken.",
+  "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+  "So many books, so little time",
+  "A room without books is like a body without a soul.",
+  "You only live once, but if you do it right, once is enough.",
+  "Be the change that you wish to see in the world.",
+  "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+  "Life is what happens when you're busy making other plans.",
+];
 
-export default qArray
+export default qArray;
